33d0474a78c3f139fbd090892ebfa2d3140dbe3b
permadihw <40470042+permadihw@users.noreply.github.com>
2018-06-22 09:17:06 +0700

    Add files via upload

---
+++ SpamPro.sh
@@ -0,0 +1,207 @@
+#!/system/xbin/bash
+
+clear
+
+toilet -f slant --gay "SpamPro"
+
+sleep 1
+
+echo "\033[31;1m Tools Spam"
+
+sleep 1
+
+echo "\033[33;1m Pembuat: PermadiHw"
+
+sleep 1
+
+echo "\033[32;1m •Wa: +6285235133797"
+
+sleep 1
+
+echo "\033[34;1m •TKJ MEDIA"
+
+sleep 1
+
+echo "\033[36;1m list Spam :"
+
+echo "1. Tokopedia"
+
+echo "2. Telkomsel"
+
+echo "3. Matahari Mall"
+
+echo "4. PHD"
+
+echo "5. JDID"
+
+echo "6. Email Bomb"
+
+echo "0. Keluar"
+
+echo "\033[30;1m81. Tentang Tool Ini"
+
+echo "\033[33;1m Pilih Angka:"
+
+read mrrm
+
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+
+then
+
+clear
+
+echo "\033[34;1m"
+
+figlet "tokopedia"
+
+php 1.php
+
+fi
+
+
+if
+
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+
+then
+
+clear
+
+echo "\033[31;1m"
+
+toilet "T-Sel"
+
+php 2.php
+
+fi
+
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+
+then
+
+clear
+
+echo "\033[31;1m"
+
+figlet "Mthr Mall"
+
+php 3.php
+
+fi
+
+
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+
+then
+
+clear
+
+toilet -f mono12 -F gay "PHD"
+
+echo "\033[30;1m"
+
+php 4.php
+
+fi
+
+
+if
+
+[ $mrrm = 5 ] || [ $mrrm = 5 ]
+
+then
+
+clear
+
+toilet -f mono12 -F gay "Jd.Id"
+
+echo "\033[33;1m"
+
+php 5.php
+
+fi
+
+
+if
+
+[ $mrrm = 6 ] || [ $mrrm = 6 ]
+
+then
+
+clear
+
+toilet -f standard -F gay "Email Bomber"
+
+echo "\033[36;1m"
+
+python2 6.py
+
+fi
+
+
+if
+
+[ $mrrm = 81 ] || [ $mrrm = 81 ]
+
+then
+
+clear
+
+toilet -f slant --gay "SpamPro"
+
+echo "\033[31;1mNama tools: SpamPro"
+
+sleep 1
+
+echo "\033[33;1mKarya: PermadiHw"
+
+sleep 1
+
+echo "\033[32;1mVersi: v2"
+
+sleep 1
+
+echo "\033[36;1mTeam: •TKJ MERIA•"
+
+sleep 1
+
+echo "\033[34;1mInformasi Lebih Lanjut Hubungi: +6285235133797"
+
+echo "\033[31;1m Spesial Thanks To: "
+
+echo "\033[36;1m"
+
+echo "SMKN NGADIROJO"
+
+sleep 1
+
+echo "TKJ MEDIA"
+
+echo "\033[30;1m tunggu 8 detik"
+
+sleep 8
+
+sh SpamPro.sh
+
+fi
+
+
+if
+
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+
+then
+
+echo "\033[31;1m Keluar"
+
+sleep 1
+
+echo "\033[32;1m Terima Kasih"
+
+sleep 1
+
+fi
+
+
